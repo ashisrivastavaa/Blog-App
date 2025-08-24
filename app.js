22407a4d1d1e08684fab469cb8de2a97faec18a3
Ashi Srivastava <ashisrivastava88@gmail.com>
2025-08-25 01:41:48 +0530

    A simple Blog app using Node.js, Express, MongoDB, and EJS
    A simple Blog app using Node.js, Express, MongoDB, and EJS

---
+++ app.js
@@ -0,0 +1,151 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
+
+const userModel = require('./models/user');
+const postModel = require('./models/post');
+const post = require('./models/post');
+
+app.set("view engine", 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+
+// Home
+app.get("/", (req, res) => {
+    res.render("index");
+});
+app.get("/register", (req, res) => {
+    res.render("index");
+});
+
+// Profile
+app.get("/profile", isLoggedIn, async (req, res) => {
+    let user = await userModel.findOne({ email: req.user.email }).populate("posts");
+    res.render("profile", { user, posts: user.posts });
+});
+app.get("/like/:id", isLoggedIn, async (req, res) => {
+    let post = await postModel.findOne({ _id: req.params.id }).populate("user");
+
+    if (!post) return res.send("Post not found");
+
+    let likeIndex = post.likes.indexOf(req.user.userid);
+
+    if (likeIndex === -1) {
+        // agar like nahi kiya toh push karega
+        post.likes.push(req.user.userid);
+    } else {
+        // agar already like hai toh remove karega (unlike)
+        post.likes.splice(likeIndex, 1);
+    }
+
+    await post.save();
+
+    res.redirect("/profile");
+});
+
+app.get("/edit/:id", isLoggedIn, async (req, res) => {
+    let post = await postModel.findOne({ _id: req.params.id }).populate("user");
+    res.render("edit",{post})
+
+});
+app.post("/update/:id", isLoggedIn, async (req, res) => {
+    let post = await postModel.findByIdAndUpdate({ _id: req.params.id },{content:req.body.content});
+    res.redirect('/profile');
+
+});
+
+
+// Create new post
+app.post("/profile", isLoggedIn, async (req, res) => {
+    let user = await userModel.findOne({ email: req.user.email });
+    let { content } = req.body;
+
+    let post = await postModel.create({
+        user: user._id,
+        content
+    });
+
+    user.posts.push(post._id);
+    await user.save();
+
+    res.redirect("/profile");
+});
+
+
+// Register
+
+ app.post("/register", async (req, res) => {
+    let { email, password, username, name, age } = req.body;
+
+    let user = await userModel.findOne({ email });
+    if (user) return res.status(500).send("User already registered");
+
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(password, salt, async (err, hash) => {
+            let newUser = await userModel.create({
+                username,
+                email,
+                age,
+                name,
+                password: hash
+            });
+
+            let token = jwt.sign({ email: email, userid: newUser._id }, "shhhhhhhh");
+            res.cookie("token", token);
+            res.send("registered");
+        });
+    });
+});
+
+
+// Show login page
+app.get("/login", (req, res) => {
+    res.render("login");   
+});
+
+// Login logic
+app.post("/login", async (req, res) => {
+    let { email, password } = req.body;
+
+    let user = await userModel.findOne({ email });
+    if (!user) return res.status(400).send("Email or Password is Wrong");
+
+    bcrypt.compare(password, user.password, function (err, result) {
+        if (result) {
+            let token = jwt.sign({ email: user.email, userid: user._id }, "shhhhhhhh");
+            res.cookie("token", token);
+            res.redirect("/profile");
+        } else {
+            res.status(400).send("Email or Password is Wrong");
+        }
+    });
+});
+
+// Logout
+app.get('/logout', (req, res) => {
+    res.cookie("token", "");  // token clear
+    res.redirect("/login");
+});
+
+// Middleware for protected routes
+function isLoggedIn(req, res, next) {
+    let token = req.cookies.token;
+    if (!token) return res.redirect("/login");
+
+    try {
+        let data = jwt.verify(token, "shhhhhhhh");
+        req.user = data;
+        next();
+    } catch (err) {
+        return res.send("Invalid Token, please login again!");
+    }
+}
+
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
